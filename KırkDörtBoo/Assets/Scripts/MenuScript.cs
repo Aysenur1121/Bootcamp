d9645dbde14b535e780ae0c92c4684cb318e48f0
Aysenur1121 <nurero1121@gmail.com>
2023-07-02 15:18:56 +0300

    Slider eklendi

--- KırkDörtBoo/Assets/Scripts/MenuScript.cs
+++ KırkDörtBoo/Assets/Scripts/MenuScript.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuScript : MonoBehaviour
 {
@@ -18,6 +19,45 @@ public class MenuScript : MonoBehaviour
     {
         Debug.Log("QUIT!!");
         Application.Quit(); //build edildiğinde çalışır, oyunu kapatacak
+    }
+    public void GoMenuButton()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public Text Volume;
+    public Slider slider;
+
+    private void Start()
+    {
+        LoadAudio();
+    }
+
+    public void SetAudio(float ses)
+    {
+        AudioListener.volume = ses;
+        Volume.text = ((int)(ses * 100)).ToString(); // texte yazması için stringe çevirdik
+        SaveAudio();
+    }
+    private void SaveAudio()
+    {
+        PlayerPrefs.SetFloat("audVol", AudioListener.volume);
+    }
+    private void LoadAudio()
+    {
+        if (PlayerPrefs.HasKey("audVol"))
+        {
+            AudioListener.volume = PlayerPrefs.GetFloat("audVol");
+            slider.value = PlayerPrefs.GetFloat("audVol");
+
+        }
+        else
+        {
+            PlayerPrefs.SetFloat("audVol", 0.5f);
+            AudioListener.volume = PlayerPrefs.GetFloat("audVol");
+            slider.value = PlayerPrefs.GetFloat("audVol");
+        }
+        
     }
     
 }
